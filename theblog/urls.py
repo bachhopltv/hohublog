d2fa22e6a5c1a2753a168fef2f5642371ff1128f
Thi Bach Hop Le <bachhopltv@gmail.com>
2021-10-06 20:28:00 +0700

    ewherq

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, AddPostView, UpdatePostView
+from .views import HomeView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
     path('add_post/', AddPostView.as_view(), name="add_post"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/edit/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
 ]
--- theblog/views.py
+++ theblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 class HomeView(ListView):
 	model = Post
@@ -16,6 +17,11 @@ class UpdatePostView(UpdateView):
 	model = Post
 	form_class = EditForm
 	template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+	model = Post
+	template_name = 'delete_post.html'
+	success_url = reverse_lazy('home')
 	
 
 
