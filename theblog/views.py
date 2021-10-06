cd0c6a1b2f3dd54eabecbc7d81c8ea5a7ef35f78
Thi Bach Hop Le <bachhopltv@gmail.com>
2021-10-06 18:08:30 +0700

    egewg

--- theblog/views.py
+++ theblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, CreateView
 from .models import Post
 
 #def home(request):
@@ -9,7 +9,9 @@ class HomeView(ListView):
 	model = Post
 	template_name = 'home.html'
 
-class ArticleDetailView(DetailView):
+class AddPostView(CreateView):
 	model = Post
-	template_name = 'article_details.html'
+	template_name = 'add_post.html'
+	fields = '__all__'
+
 
