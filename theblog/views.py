2a4ab0c798c48ea8d4dd0fc37d73977e20d1edde
Thi Bach Hop Le <bachhopltv@gmail.com>
2021-10-06 19:58:56 +0700

    Gqeh

--- theblog/views.py
+++ theblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Post
+from .forms import PostForm
 
 class HomeView(ListView):
 	model = Post
@@ -8,7 +9,8 @@ class HomeView(ListView):
 
 class AddPostView(CreateView):
 	model = Post
+	form_class = PostForm
 	template_name = 'add_post.html'
-	fields = '__all__'
+
 
 
