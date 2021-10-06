1bbd34f2078827ad01a7c1feb9c3a5b1c4ceb639
Thi Bach Hop Le <bachhopltv@gmail.com>
2021-10-06 20:02:39 +0700

    qerhqr

--- theblog/forms.py
+++ theblog/forms.py
@@ -5,7 +5,7 @@ class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('title', 'author', 'body')
-		widget = {
+		widgets = {
 			'title': forms.TextInput(attrs={'class': 'form-control'}),
 			'author': forms.Select(attrs={'class': 'form-control'}),
 			'body': forms.Textarea(attrs={'class': 'form-control'}),
