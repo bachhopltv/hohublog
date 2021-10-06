d5262808b283329f1204859a2acccaad74f25e9b
Thi Bach Hop Le <bachhopltv@gmail.com>
2021-10-06 12:55:35 +0700

    fwefe

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 #from . import views
 from .views import HomeView, ArticleDetailView
+from .models import Post
 
 urlpatterns = [
     #path('', views.home, name="home"),
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(queryset=Post.objects.all()), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
 ]
