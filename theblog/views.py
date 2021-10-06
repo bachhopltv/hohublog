7b02a9094defe1faee58ca267168242f072f3f94
Thi Bach Hop Le <bachhopltv@gmail.com>
2021-10-06 20:49:12 +0700

    Hehwehq

--- theblog/views.py
+++ theblog/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
 	model = Post
 	template_name = 'home.html'
-	#ordering = ['-post_date', '-id']
+	ordering = ['-post_date', '-id']
 
 class AddPostView(CreateView):
 	model = Post
